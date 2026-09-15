eab5ddb293df66aa20b494dca25bcd9359090743
bubba-L <benbenlarkin@gmail.com>
2026-09-15 11:00:23 -0400

    Link update2

--- astro.config.ts
+++ astro.config.ts
@@ -22,7 +22,7 @@ const {
 
 // https://astro.build/config
 const config = defineConfig({
-	site: "https://spectre.lou.gg",
+	site: "https://benlarkin.org",
 	output: "static",
 	integrations: [
 		expressiveCode({
